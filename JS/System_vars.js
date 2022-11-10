34e19d30bcff963f521cd99286a601ba896755dc
willmil11 <willmil1110@gmail.com>
2022-11-10 08:06:01 +0100

    building basics 1/...

---
+++ JS/System_vars.js
@@ -0,0 +1,8 @@
+//This file include the System vars of the 3DWEB library
+//3DWEB by willmil11
+//V0.1 in-dev build
+
+let BA576D5A7134743777217A25432A462D = {
+    innerHeight: (window.innerHeight + "px"),
+    innerWidth: (window.innerWidth + "px")
+}
---
+++ JS/function.js
@@ -0,0 +1,10 @@
+//This file include the function of the 3DWEB library
+//3DWEB by willmil11
+//V0.1 in-dev build
+
+function NewCanvas(variable){
+    //Set the variable to the default canvas profile
+    eval((variable + " = BA576D5A7134743777217A25432A462D"));
+    //Return true if operation suceed else return nothing (undefined)
+    return true;
+}
